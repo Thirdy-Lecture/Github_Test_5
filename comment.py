747f0efd2ae8ec5621d20103cc6fb34343c14109
thirdygayares <gayaresthird@gmail.com>
2024-08-24 18:27:34 +0800

    Add comment and datatypes

---
+++ comment.py
@@ -0,0 +1,4 @@
+# Python comment
+# This is a comment
+
+print ("Hi, I am Shamel Jane D. Francisco BSIT 2-A") #This is my name, year and section SSS
---
+++ datatypes.py
@@ -0,0 +1,11 @@
+# Python Variable
+
+name = "ROel"
+age = 18
+is_student = True
+height = 5.6
+
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", is_student)
+print("Height:", height, "feet")
